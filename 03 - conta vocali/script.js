2b23f7a71ca066b7fad1d74719d33defcb48086c
emanuele23dev <emanuele.meggiarin@libero.it>
2024-10-07 16:36:55 +0200

    end vowels

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,13 +1,16 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "ernestina";
 
 // Dichiara la funzione qui.
-
+function vocali(word) {
+  const vow = ["a", "e", "i", "o", "u"];
+  return word.split("").filter((vocali) => vow.includes(vocali)).sort();
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(vocali(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+
