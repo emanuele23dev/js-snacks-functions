73cb4780fbc5cf51e0d75056efeeb80fce1cde6d
emanuele23dev <emanuele.meggiarin@libero.it>
2024-10-07 16:52:35 +0200

    end saluti

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,12 +1,19 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
+
+// 👇 name lo indica come deprecato
 const name = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function saluto (nome) {
+return `Ciao ${nome}`
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+
+const nome = 'Mario'
+console.log(saluto(nome));
 
 
 
